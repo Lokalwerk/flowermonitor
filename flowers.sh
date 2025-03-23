8013b85732470a8acdca357cc2c8ee078ed7b523
Lokalwerk <53934849+Lokalwerk@users.noreply.github.com>
2025-03-23 21:32:06 +0100

    Update flowers.sh

--- flowers.sh
+++ flowers.sh
@@ -1,2 +1,2 @@
-celery flower --port=$PORT --broker=$BROKER_URL --basic_auth=$FLOWER_BASIC_AUTH --result-backend=$RESULT_BACKEND
+celery flower --port=9090 --broker=$BROKER_URL --basic_auth=$FLOWER_BASIC_AUTH --result-backend=$RESULT_BACKEND
 
